f49fa907b789bf31ee6c20c269110f6c01c8feec
odai-dh <odai.dahi@hyperisland.se>
2025-09-02 11:44:45 +0200

    Google tag verification code added to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,6 +84,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark scroll-smooth">
       <head>
+        <meta name="google-site-verification" content="eqIL5mctJjbAPj_179mB8g0QQvo0wCHZ6PLtqJ8MJFU" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
